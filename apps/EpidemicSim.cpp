b4e093108c26ba6d8de08004f89778c1dd2def8d
Ryan Pearson <Ryan.Pearson@jhuapl.edu>
2020-05-12 10:35:19 -0400

    - Added additional monte carlo logging

--- apps/EpidemicSim.cpp
+++ apps/EpidemicSim.cpp
@@ -36,6 +36,24 @@ int main()
       const std::string fullFilePath = SOURCE_DIR + "/test_input_small.xml";
       const auto worldConfiguration = Epidemic::get_world_config_from_xml(fullFilePath);
 
+      struct CumulativeInfo
+      {
+         double e_x = 0.0;
+         double e_x_sq = 0.0;
+         int numAdded = 0;
+      };
+
+      struct MonteCarloSirdInfo
+      {
+         CumulativeInfo s;
+         CumulativeInfo i;
+         CumulativeInfo r;
+         CumulativeInfo d;
+      };
+
+      std::vector<MonteCarloSirdInfo> sirdByDay;
+      sirdByDay.reserve(100);
+
       for (int i = 0; i < worldConfiguration.m_numMonteCarloRuns; ++i)
       {
          auto world = Epidemic::build_world(worldConfiguration);
@@ -56,6 +74,41 @@ int main()
          }
 
          const auto logStats = [&](const int day, const double curR, const Epidemic::World::SIRD_Levels& stats) {
+            if (day >= sirdByDay.size())
+            {
+               sirdByDay.emplace_back();
+            }
+
+            auto& curDay = sirdByDay[day];
+
+            curDay.s.e_x = (curDay.s.e_x * curDay.s.numAdded + static_cast<double>(stats.m_numSusceptible))
+               / (curDay.s.numAdded + 1);
+            curDay.s.e_x_sq = (curDay.s.e_x_sq * curDay.s.numAdded
+                                 + static_cast<double>(stats.m_numSusceptible * stats.m_numSusceptible))
+               / (curDay.s.numAdded + 1);
+            ++curDay.s.numAdded;
+
+            curDay.i.e_x = (curDay.i.e_x * curDay.i.numAdded + static_cast<double>(stats.m_numInfectious))
+               / (curDay.i.numAdded + 1);
+            curDay.i.e_x_sq = (curDay.i.e_x_sq * curDay.i.numAdded
+                                 + static_cast<double>(stats.m_numInfectious * stats.m_numInfectious))
+               / (curDay.i.numAdded + 1);
+            ++curDay.i.numAdded;
+
+            curDay.r.e_x =
+               (curDay.r.e_x * curDay.r.numAdded + static_cast<double>(stats.m_numRecovered)) / (curDay.r.numAdded + 1);
+            curDay.r.e_x_sq =
+               (curDay.r.e_x_sq * curDay.r.numAdded + static_cast<double>(stats.m_numRecovered * stats.m_numRecovered))
+               / (curDay.r.numAdded + 1);
+            ++curDay.r.numAdded;
+
+            curDay.d.e_x =
+               (curDay.d.e_x * curDay.d.numAdded + static_cast<double>(stats.m_numDeceased)) / (curDay.d.numAdded + 1);
+            curDay.d.e_x_sq =
+               (curDay.d.e_x_sq * curDay.d.numAdded + static_cast<double>(stats.m_numDeceased * stats.m_numDeceased))
+               / (curDay.d.numAdded + 1);
+            ++curDay.d.numAdded;
+
             std::cout << fmt::format("Day {}: S[{}] I[{}] R[{}] D[{}] R[{}]\n", day, stats.m_numSusceptible,
                stats.m_numInfectious, stats.m_numRecovered, stats.m_numDeceased, curR);
             sirdLog << day << ',' << curR << ',' << stats.m_numSusceptible << ',' << stats.m_numInfectious << ','
@@ -97,6 +150,53 @@ int main()
          }
 
          logStats(curTime / Epidemic::TIMESTEP_PER_DAY, world.get_cur_R_level(), worldStats);
+      }
+
+      std::ofstream sirdLog(OUTPUT_ROOT + fmt::format("/sirdLog_overall.csv"));
+      sirdLog << "Day,Susceptible_Mean,Susceptible_Std,Infectious_Mean,Infectious_Std,Recovered_Mean,Recovered_Std,"
+                 "Deceased_Mean,Deceased_Std,Infectious_Mean,Infectious_Low,Infectious_High,Susceptible+Infectious_"
+                 "Mean,Susceptible+Infectious_Low,Susceptible+Infectious_High,Susceptible+Infectious+Recovered_Mean,"
+                 "Susceptible+Infectious+Recovered_Low,Susceptible+Infectious+Recovered_High,Susceptible+Infectious+"
+                 "Recovered+Deceased_Mean,Susceptible+Infectious+Recovered+Deceased_Low,Susceptible+Infectious+"
+                 "Recovered+Deceased_High\n";
+
+      for (int i = 0; i < sirdByDay.size(); ++i)
+      {
+         const auto& data = sirdByDay[i];
+
+         const double e_s = data.s.e_x;
+         const double e_i = data.i.e_x;
+         const double e_r = data.r.e_x;
+         const double e_d = data.d.e_x;
+
+         const double std_s = std::sqrt(data.s.e_x_sq - data.s.e_x * data.s.e_x);
+         const double std_i = std::sqrt(data.i.e_x_sq - data.i.e_x * data.i.e_x);
+         const double std_r = std::sqrt(data.r.e_x_sq - data.r.e_x * data.r.e_x);
+         const double std_d = std::sqrt(data.d.e_x_sq - data.d.e_x * data.d.e_x);
+
+         const double a_e = e_i;
+         const double a_s = std_i;
+         const double a_l = a_e - a_s;
+         const double a_h = a_e + a_s;
+
+         const double b_e = e_i + e_s;
+         const double b_s = std::sqrt(std_i * std_i + std_s * std_s);
+         const double b_l = b_e - b_s;
+         const double b_h = b_e + b_s;
+
+         const double c_e = e_i + e_s + e_r;
+         const double c_s = std::sqrt(std_i * std_i + std_s * std_s + std_r * std_r);
+         const double c_l = c_e - c_s;
+         const double c_h = c_e + c_s;
+
+         const double d_e = e_i + e_s + e_r + e_d;
+         const double d_s = std::sqrt(std_i * std_i + std_s * std_s + std_r * std_r + std_d * std_d);
+         const double d_l = d_e - d_s;
+         const double d_h = d_e + d_s;
+
+         sirdLog << i << e_s << ',' << std_s << ',' << e_i << ',' << std_i << ',' << e_r << ',' << std_r << ',' << e_d
+                 << ',' << std_d << ',' << a_e << ',' << a_l << ',' << a_h << ',' << b_e << ',' << b_l << ',' << b_h
+                 << ',' << c_e << ',' << c_l << ',' << c_h << ',' << d_e << ',' << d_l << ',' << d_h << '\n';
       }
 
       std::cout << "Simulation complete" << std::endl;
